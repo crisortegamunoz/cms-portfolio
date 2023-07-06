99d4a4e9efb937b712b6d91e8dc365a96275ea1b
Cristian Ortega <crisortegamunoz@gmail.com>
2023-07-05 23:19:36 -0400

    eliminacion componentes y libreria de excel

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -2,6 +2,4 @@ export * from './shared.module';
 export { MaterialModule } from './material.module';
 export { FeatherIconsModule } from './feather-icons.module';
 export { SubSink } from './sub-sink';
-export { TableElement } from './TableElement';
-export { TableExportUtil } from './tableExportUtil';
 export { UnsubscribeOnDestroyAdapter } from './UnsubscribeOnDestroyAdapter';
